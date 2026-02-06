dcd3a4c3e7343ff615ff91465e10f9b445a08515
codeAkhilesh14 <av384783@gmail.com>
2026-02-06 18:43:50 +0530

    Update redis.ts

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -19,6 +19,7 @@
 // redis.on("error", (err) => {
 //   console.error("Redis connection error:", err);
 // });
+
 import Redis from "ioredis";
 import dotenv from "dotenv";
 
@@ -27,16 +28,30 @@ dotenv.config();
 let redis: Redis;
 
 if (process.env.REDIS_URL) {
-  // PRODUCTION (Render)
+  // PRODUCTION (Render Key-Value)
   redis = new Redis(process.env.REDIS_URL);
-  console.log("Connected to Redis via REDIS_URL");
+
+  console.log("Connected to Redis using REDIS_URL");
 } else {
   // LOCAL DEVELOPMENT
   redis = new Redis({
-    host: process.env.REDIS_HOST,
-    port: Number(process.env.REDIS_PORT),
+    host: process.env.REDIS_HOST || "localhost",
+    port: Number(process.env.REDIS_PORT) || 6379,
+    retryStrategy(times) {
+      const delay = Math.min(times * 50, 2000);
+      return delay;
+    }
   });
+
   console.log("Connected to local Redis");
 }
+
+redis.on("connect", () => {
+  console.log("Redis connection successful");
+});
+
+redis.on("error", (err) => {
+  console.error("Redis connection error:", err);
+});
 
 export { redis };
